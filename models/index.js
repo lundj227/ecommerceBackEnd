5c2b46054cef0b638dc1b44f84cdc1794d3cafd7
Jack Lund <jacklund@Jacks-MacBook-Pro.local>
2023-12-27 17:18:28 -0800

    made the models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'id'
+});
 // Tags belongToMany Products (through ProductTag)
+Tags.belongToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
